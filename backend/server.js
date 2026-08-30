31d9a22837df9242e26f87e956651dd90d28c3eb
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-08-31 00:49:14 +0530

    start server and check db connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config({ path: './.env' });
+import connectDB from "./src/db/db.connect.js";
+import app from "./src/app.js";
 
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("Database connection failed", error);
+    })
+
+
+    
