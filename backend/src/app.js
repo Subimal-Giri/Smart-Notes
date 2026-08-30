6acb5d4ca41b55ab2a3e7b83cc28674112d25191
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-08-31 00:47:37 +0530

    connect and use express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
+
+
+app.get("/api/v1/health", (req, res) => {
+    res.status(200).json({ success: true, message: "SmartNotes API is running", timestamp: new Date() });
+});
+
+
+export default app;
