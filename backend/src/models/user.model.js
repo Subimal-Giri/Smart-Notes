823b5a4c2dfb9ae720227b9c4ce31252c9d26672
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-08-31 01:01:02 +0530

    add user schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        }
+    },
+    { timestamps: true }
+)
+
+
+
+export const User = mongoose.model("User", userSchema)
+
