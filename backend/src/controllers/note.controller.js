147409bf74858425fea889f22948d68669d5c632
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-03 00:53:12 +0530

    add create note & get note by Id controller

---
+++ backend/src/controllers/note.controller.js
@@ -0,0 +1,103 @@
+import { Note } from "../models/note.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
+
+const isValidId = (id) => mongoose.Types.ObjectId.isValid(id);
+
+const getAllNotes = asyncHandler(async (req, res) => {
+    
+});
+
+const getArchived = asyncHandler(async (req, res) => {
+    
+});
+
+const getTrashed = asyncHandler(async (req, res) => {
+    
+});
+
+const searchNotes = asyncHandler(async (req, res) => {
+    
+});
+
+const getById = asyncHandler(async (req, res) => {
+    if (!isValidId(req.params.id)) {
+        throw new ApiError(400, "Invalid note id");
+    }
+
+    const note = await Note.findOne({
+        _id: req.params.id,
+        user: req.user._id
+    }).populate("tags");
+
+    if (!note) {
+        throw new ApiError(404, "Note not found");
+    }
+    return res.status(200).json(
+        new ApiResponse(200, note, "Note fetched")
+    );
+});
+
+const createNote = asyncHandler(async (req, res) => {
+    const { title, content, tags } = req.body;
+
+    const note = await Note.create({
+        user: req.user._id,
+        title,
+        content,
+        tags
+    });
+
+    await note.populate("tags");
+
+    return res.status(201).json(
+        new ApiResponse(201, note, "Note created")
+    );
+});
+
+const updateNote = asyncHandler(async (req, res) => {
+
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+
+});
+
+const trashNote = asyncHandler(async (req, res) => {
+
+});
+
+const restoreNote = asyncHandler(async (req, res) => {
+
+});
+
+const pinNote = asyncHandler(async (req, res) => {
+
+});
+
+const archiveNote = asyncHandler(async (req, res) => {
+
+});
+
+const syncTags = asyncHandler(async (req, res) => {
+
+});
+
+export {
+    getAllNotes,
+    getArchived,
+    getTrashed,
+    searchNotes,
+    getById,
+    createNote,
+    updateNote,
+    deleteNote,
+    trashNote,
+    restoreNote,
+    pinNote,
+    archiveNote,
+    syncTags
+}
+
