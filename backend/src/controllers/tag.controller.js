69a640079278f4a3b28bcb1c61d2f201f5c5fd5f
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-04 16:31:42 +0530

    create tag controller structure

---
+++ backend/src/controllers/tag.controller.js
@@ -0,0 +1,25 @@
+import { Tag } from "../models/tag.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+
+const createTag = asyncHandler(async (req, res) => {
+    
+});
+
+const getAllTags = asyncHandler(async (req, res) => {
+    
+});
+
+const updateTag = asyncHandler(async (req, res) => {
+
+});
+
+const deleteTag = asyncHandler(async (req, res) => {
+
+});
+
+
+export { createTag, getAllTags, updateTag, deleteTag }
+
