44cbafaae9f2c7d92c6974b45dbc6b7231e5a5d0
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-04 16:38:53 +0530

    add create Tag controller

--- backend/src/controllers/tag.controller.js
+++ backend/src/controllers/tag.controller.js
@@ -5,7 +5,21 @@ import { ApiError } from "../utils/ApiError.js";
 
 
 const createTag = asyncHandler(async (req, res) => {
-    
+    const { tagName, color } = req.body;
+
+    if (!tagName) {
+        throw new ApiError(400, "Tag name is required");
+    }
+
+    const tag = await Tag.create({
+        user: req.user._id,
+        tagName,
+        color
+    });
+
+    return res.status(201).json(
+        new ApiResponse(201, tag, "New Tag created")
+    );
 });
 
 const getAllTags = asyncHandler(async (req, res) => {
