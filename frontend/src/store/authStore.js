e97ba10fd1b0a85991a456f8137a89722f13a3b3
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-07 21:14:17 +0530

    add use auth store

---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export const useAuthStore = create(
+    persist((set) => ({
+            user: null,
+            accessToken: null,
+            isAuthenticated: false,
+            setAuth: (user, accessToken) => set({ user, accessToken, isAuthenticated: true }),
+            setUser: (user) => set({ user }),
+            logout: () => set({ user: null, accessToken: null, isAuthenticated: false }),
+        }),
+        {name: "smartnotes-auth"}
+    )
+);
+
