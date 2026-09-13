c6240a259253c504d616e2d6504c2619ba15ef68
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-13 23:45:21 +0530

    create Landing page structure and add Navbar, Footer.

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+
+import LandingNavbar from "../components/landing/LandingNavbar";
+import LandingFooter from "../components/landing/LandingFooter";
+
+function LandingPage() {
+    return (
+        <div className="landing">
+            <LandingNavbar />
+        
+            <LandingFooter />
+        </div>
+    );
+}
+
+export default LandingPage;
+
