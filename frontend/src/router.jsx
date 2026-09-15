469734885146b03d1e2069370c464cf750276a89
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-16 00:34:52 +0530

    create Landing page route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,1 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+
+import LandingPage from "./pages/LandingPage.jsx";
+
+function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+
+        </Routes>
+    );
+}
+
+export default AppRouter;
 
