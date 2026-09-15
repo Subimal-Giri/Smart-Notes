e1c349bee2f65c6aab26cfa04d156b638c79cb2c
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-15 15:36:52 +0530

    add Hero, FeaturesSection, HowItWorks, CTASection in Landing page.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,18 @@
-
 import LandingNavbar from "../components/landing/LandingNavbar";
+import Hero from "../components/landing/Hero";
+import FeaturesSection from "../components/landing/FeaturesSection";
+import HowItWorks from "../components/landing/HowItWorks";
+import CTASection from "../components/landing/CTASection";
 import LandingFooter from "../components/landing/LandingFooter";
 
 function LandingPage() {
     return (
         <div className="landing">
             <LandingNavbar />
-        
+            <Hero />
+            <FeaturesSection />
+            <HowItWorks />
+            <CTASection />        
             <LandingFooter />
         </div>
     );
