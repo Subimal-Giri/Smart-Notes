dc7547d922eb3ae95b2c4d2fe1152c368d0ae3a0
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-17 21:25:40 +0530

    create login, register route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,11 +4,23 @@ import PublicOnlyRoute from "./components/auth/PublicOnlyRoute";
 
 
 import LandingPage from "./pages/LandingPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+
 
 function AppRouter() {
     return (
         <Routes>
             <Route path="/" element={<LandingPage />} />
+
+            <Route element={<PublicOnlyRoute />}>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+            </Route>
+
+            <Route element={<ProtectedRoute />}>
+        
+            </Route>
 
         </Routes>
     );
