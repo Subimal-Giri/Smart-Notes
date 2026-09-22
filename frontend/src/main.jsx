9d13a0c5cf4ed2053aba12dc9422679b8e2f3bcc
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-22 14:45:44 +0530

    add ThemeProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from "sonner";
 import App from './App.jsx'
-
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import "./styles/global.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
-      <Toaster position="top-right" richColors closeButton />
+      <ThemeProvider>
+        <App />
+        <Toaster position="top-right" richColors closeButton />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
