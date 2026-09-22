ec1d3e755e7ab02749251bf025db1d75cfdb8af6
Subimal-Giri <girisubimalgiri975@gmail.com>
2026-09-22 14:45:26 +0530

    add dashboard route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import PublicOnlyRoute from "./components/auth/PublicOnlyRoute";
-
+import ProtectedRoute from "./components/auth/ProtectedRoute.jsx";
+import PublicOnlyRoute from "./components/auth/PublicOnlyRoute.jsx";
+import MainLayout from "./components/layout/MainLayout.jsx";
 
 import LandingPage from "./pages/LandingPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
-
+import DashboardPage from "./pages/DashboardPage.jsx";
 
 function AppRouter() {
     return (
@@ -19,7 +19,9 @@ function AppRouter() {
             </Route>
 
             <Route element={<ProtectedRoute />}>
-        
+                <Route element={<MainLayout />}>
+                    <Route path="/dashboard" element={<DashboardPage />} />
+                </Route>
             </Route>
 
         </Routes>
